da3806e346005741d602a1731df9d2232cb1cdac
h0g3ria <72096929+h0g3ria@users.noreply.github.com>
2026-08-09 02:03:53 +0900

    Fix account verification authentication bypass

--- src/main/java/org/owasp/webgoat/lessons/authbypass/AccountVerificationHelper.java
+++ src/main/java/org/owasp/webgoat/lessons/authbypass/AccountVerificationHelper.java
@@ -55,26 +55,7 @@ public class AccountVerificationHelper {
   // end of cheating check ... the method below is the one of real interest. Can you find the flaw?
 
   public boolean verifyAccount(Integer userId, HashMap<String, String> submittedQuestions) {
-    // short circuit if no questions are submitted
-    if (submittedQuestions.entrySet().size() != secQuestionStore.get(verifyUserId).size()) {
-      return false;
-    }
-
-    if (submittedQuestions.containsKey("secQuestion0")
-        && !submittedQuestions
-            .get("secQuestion0")
-            .equals(secQuestionStore.get(verifyUserId).get("secQuestion0"))) {
-      return false;
-    }
-
-    if (submittedQuestions.containsKey("secQuestion1")
-        && !submittedQuestions
-            .get("secQuestion1")
-            .equals(secQuestionStore.get(verifyUserId).get("secQuestion1"))) {
-      return false;
-    }
-
-    // else
-    return true;
+    Map expectedQuestions = secQuestionStore.get(userId);
+    return expectedQuestions != null && expectedQuestions.equals(submittedQuestions);
   }
 }
