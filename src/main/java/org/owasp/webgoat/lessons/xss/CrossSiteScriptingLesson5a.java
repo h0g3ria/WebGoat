2f49de2e3bcd95c4edf9345b83cf7ae94066be03
h0g3ria <72096929+h0g3ria@users.noreply.github.com>
2026-08-09 02:32:13 +0900

    fix(xss): encode reflected credit card input

--- src/main/java/org/owasp/webgoat/lessons/xss/CrossSiteScriptingLesson5a.java
+++ src/main/java/org/owasp/webgoat/lessons/xss/CrossSiteScriptingLesson5a.java
@@ -4,6 +4,7 @@
  */
 package org.owasp.webgoat.lessons.xss;
 
+import static org.apache.commons.text.StringEscapeUtils.escapeHtml4;
 import static org.owasp.webgoat.container.assignments.AttackResultBuilder.failed;
 import static org.owasp.webgoat.container.assignments.AttackResultBuilder.success;
 
@@ -62,7 +63,7 @@ public class CrossSiteScriptingLesson5a implements AssignmentEndpoint {
     userSessionData.setValue("xss-reflected1-complete", "false");
     StringBuilder cart = new StringBuilder();
     cart.append("Thank you for shopping at WebGoat. <br />Your support is appreciated<hr />");
-    cart.append("<p>We have charged credit card:" + field1 + "<br />");
+    cart.append("<p>We have charged credit card:" + escapeHtml4(field1) + "<br />");
     cart.append("                             ------------------- <br />");
     cart.append("                               $" + totalSale);
 
