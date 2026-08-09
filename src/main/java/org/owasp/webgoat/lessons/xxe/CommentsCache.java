f116fc6eaf959622ecd56338e67d70a94686e6aa
h0g3ria <72096929+h0g3ria@users.noreply.github.com>
2026-08-09 15:09:04 +0900

    Secure XML parsing for content-type assignment

--- src/main/java/org/owasp/webgoat/lessons/xxe/CommentsCache.java
+++ src/main/java/org/owasp/webgoat/lessons/xxe/CommentsCache.java
@@ -70,8 +70,9 @@ public class CommentsCache {
     var jc = JAXBContext.newInstance(Comment.class);
     var xif = XMLInputFactory.newInstance();
 
-    // TODO fix me disabled for now.
     if (securityEnabled) {
+      xif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+      xif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
       xif.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, ""); // Compliant
       xif.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, ""); // compliant
     }
