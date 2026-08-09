0f89afbe87c2c723d1f562e457a0efdc82204dbf
h0g3ria <72096929+h0g3ria@users.noreply.github.com>
2026-08-09 15:32:55 +0900

    Fix SQL injection in input validation lesson

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/mitigation/SqlOnlyInputValidation.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/mitigation/SqlOnlyInputValidation.java
@@ -6,10 +6,16 @@ package org.owasp.webgoat.lessons.sqlinjection.mitigation;
 
 import static org.owasp.webgoat.container.assignments.AttackResultBuilder.failed;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import org.owasp.webgoat.container.LessonDataSource;
 import org.owasp.webgoat.container.assignments.AssignmentEndpoint;
 import org.owasp.webgoat.container.assignments.AssignmentHints;
 import org.owasp.webgoat.container.assignments.AttackResult;
-import org.owasp.webgoat.lessons.sqlinjection.advanced.SqlInjectionLesson6a;
+import org.owasp.webgoat.lessons.sqlinjection.introduction.SqlInjectionLesson5a;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,10 +26,11 @@ import org.springframework.web.bind.annotation.RestController;
     value = {"SqlOnlyInputValidation-1", "SqlOnlyInputValidation-2", "SqlOnlyInputValidation-3"})
 public class SqlOnlyInputValidation implements AssignmentEndpoint {
 
-  private final SqlInjectionLesson6a lesson6a;
+  private static final String QUERY = "SELECT * FROM user_data WHERE last_name = ?";
+  private final LessonDataSource dataSource;
 
-  public SqlOnlyInputValidation(SqlInjectionLesson6a lesson6a) {
-    this.lesson6a = lesson6a;
+  public SqlOnlyInputValidation(LessonDataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
   @PostMapping("/SqlOnlyInputValidation/attack")
@@ -32,14 +39,29 @@ public class SqlOnlyInputValidation implements AssignmentEndpoint {
     if (userId.contains(" ")) {
       return failed(this).feedback("SqlOnlyInputValidation-failed").build();
     }
-    AttackResult attackResult = lesson6a.injectableQuery(userId);
-    return new AttackResult(
-        attackResult.isLessonCompleted(),
-        attackResult.getFeedback(),
-        attackResult.getFeedbackArgs(),
-        attackResult.getOutput(),
-        attackResult.getOutputArgs(),
-        getClass().getSimpleName(),
-        true);
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement =
+            connection.prepareStatement(
+                QUERY, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+      statement.setString(1, userId);
+      try (ResultSet results = statement.executeQuery()) {
+        if (!results.first()) {
+          return failed(this)
+              .feedback("sql-injection.advanced.6a.no.results")
+              .output("<br> Your query was: " + QUERY)
+              .build();
+        }
+
+        ResultSetMetaData resultsMetaData = results.getMetaData();
+        return failed(this)
+            .output(
+                SqlInjectionLesson5a.writeTable(results, resultsMetaData)
+                    + "<br> Your query was: "
+                    + QUERY)
+            .build();
+      }
+    } catch (SQLException e) {
+      return failed(this).output(e.getMessage() + "<br> Your query was: " + QUERY).build();
+    }
   }
 }
